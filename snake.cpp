f812dac79b32a765b41e6d5ccd5b24050330617a
guptaaryan1524 <guptaaryan1524@github.com>
2022-07-23 23:48:23 +0400

    code complete but infinite looping

--- snake.cpp
+++ snake.cpp
@@ -1,9 +1,44 @@
-#include <conio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <iostream>
+#include <sys/ioctl.h>
+#include <termios.h>
 
-int height = 50, width = 50, score;
+// Had to find and copy functions for getch() and kbhit() as
+// conio.h is not POSIX compatible and did not run properly on my system
+
+bool kbhit()
+{
+    termios term;
+    tcgetattr(0, &term);
+
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+
+    tcsetattr(0, TCSANOW, &term);
+
+    return byteswaiting > 0;
+}
+
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+int height = 20, width = 20, score;
 int snakex, snakey, fruitx, fruity;
 char dir;
 bool gameover;
@@ -15,14 +50,14 @@ void generation()
     snakey = width / 2;
 
 fruithor:
-    fruitx = rand() % 50;
+    fruitx = rand() % 20;
     if (fruitx == 0)
     {
         goto fruithor;
     }
 
 fruitver:
-    fruity = rand() % 50;
+    fruity = rand() % 20;
     if (fruity == 0)
     {
         goto fruitver;
@@ -33,28 +68,28 @@ fruitver:
 
 void boundaries()
 {
-    system("cls");
+    system("clear");
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width; ++j)
         {
-            if (i == 0 || j = 0 || i == width - 1 || j == height - 1)
+            if (i == 0 || j == 0 || i == (width - 1) || j == (height - 1))
             {
                 printf("%s", "X");
             }
             else if (i == snakex && j == snakey)
             {
-                printf("%s", "<");
+                printf("%s", "0");
             }
             else if (i == fruitx && j == fruity)
             {
-                printf("%s", "0");
+                printf("%s", "*");
             }
             else
             {
-                printf("");
+                printf("%s", "");
             }
-            printf("/n");
+            printf("\n");
         }
     }
 }
@@ -80,8 +115,6 @@ void keys()
         case 'x':
             gameover = true;
             break;
-        default:
-            continue;
         }
     }
 }
@@ -92,22 +125,22 @@ void movement()
     switch (dir)
     {
     case 'u':
-        --x;
+        --snakex;
         break;
     case 'l':
-        --y;
+        --snakey;
         break;
     case 'd':
-        ++x;
+        ++snakex;
         break;
     case 'r':
-        ++y;
+        ++snakey;
         break;
     default:
         break;
     }
 
-    if (x < 0 || x > height || y < 0 || y > width)
+    if (snakex < 0 || snakex > height || snakey < 0 || snakey > width)
     {
         gameover = true;
     }
@@ -115,13 +148,13 @@ void movement()
     if (snakex == fruitx && snakey == fruity)
     {
     fruitinitx:
-        fruitx = rand() % 50;
+        fruitx = rand() % 20;
         if (fruitx == 0)
         {
             goto fruitinitx;
         }
     fruitinity:
-        fruity = rand() % 50;
+        fruity = rand() % 20;
         if (fruity == 0)
         {
             goto fruitinity;
@@ -130,6 +163,14 @@ void movement()
     }
 }
 
-void main()
+int main()
 {
+    generation();
+    while (!gameover)
+    {
+        boundaries();
+        keys();
+        movement();
+    }
+    return 0;
 }
